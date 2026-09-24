33ee7321ef1ac9823530bc1e19a154c91ad5ae8c
cinenestmedia-creator <cinenestmedia@gmail.com>
2026-09-24 14:49:36 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
+  base: "/cinenest-portfolio/",
+
   tanstackStart: {
     server: { entry: "server" },
 
