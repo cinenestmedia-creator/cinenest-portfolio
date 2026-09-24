22128432719edbb4007bb95cba360bad54abfd85
cinenestmedia-creator <cinenestmedia@gmail.com>
2026-09-24 14:29:41 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/cinenest-portfolio/",
-  },
-
   tanstackStart: {
     server: { entry: "server" },
+
+    prerender: {
+      enabled: true,
+      autoSubfolderIndex: true,
+      autoStaticPathsDiscovery: true,
+      crawlLinks: true,
+    },
   },
 });
